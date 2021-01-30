e3c48cea4f41a1537a381f946185a267bcbcf2a5
vettiebinyjarV@teknuance.com <ebinyjar>
2021-01-30 14:22:35 +0530

    postman collection
    Signed-off-by: vettiebinyjarV@teknuance.com <ebinyjar>

---
+++ sampldata.js
@@ -0,0 +1,50 @@
+retriveFileDataWithTags = [
+    {
+        "_id": "6012ce17d569e734be7abe7f",
+        "tags": [
+            "JGH"
+        ],
+        "fileName": "dump.rdb",
+        "fileType": "application/octet-stream",
+        "filePath": "/home/vetti/documentmanager/public/dump.rdb",
+        "__v": 0,
+        "createdAt": "2021-01-28T14:45:43.256Z",
+        "updatedAt": "2021-01-28T14:45:43.256Z"
+    },
+    {
+        "_id" : "6012ce17d569e734be7abe7f",
+        "tags" : [ 
+            "JGH"
+        ],
+        "fileName" : "myimage.rdb",
+        "fileType" : "application/octet-stream",
+        "filePath" : "/home/vetti/documentmanager/public/myimage.rdb",
+        "__v" : 0,
+        "createdAt" : "2021-01-28T14:45:43.256Z",
+        "updatedAt" : "2021-01-28T14:45:43.256Z"
+    },
+    {
+        "_id": "6012ce17d569e734be7abe7f",
+        "tags": [
+            "JGH"
+        ],
+        "fileName": "dump.rdb",
+        "fileType": "application/octet-stream",
+        "filePath": "/home/vetti/documentmanager/public/dump.rdb",
+        "__v": 0,
+        "createdAt": "2021-01-28T14:45:43.256Z",
+        "updatedAt": "2021-01-28T14:45:43.256Z"
+    },
+    {
+        "_id" : "6012ce17d569e734be7abe7f",
+        "tags" : [ 
+            "JGH"
+        ],
+        "fileName" : "corttoon.jpg",
+        "fileType" : "application/octet-stream",
+        "filePath" : "/home/vetti/documentmanager/public/corttoon.jpg.",
+        "__v" : 0,
+        "createdAt" : "2021-01-28T14:45:43.256Z",
+        "updatedAt" : "2021-01-28T14:45:43.256Z"
+    }
+]
